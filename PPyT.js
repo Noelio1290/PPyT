7e97b4a30ccf67a1f51974c834f4fcf55e54e2fa
Noelio1290 <noelio1290@gmail.com>
2022-09-03 00:09:32 -0500

    Agrego stylos con selectores en css

--- PPyT.js
+++ PPyT.js
@@ -1,13 +1,11 @@
 function jugarPPyT(posibilidad1,posibilidad2) {
     if (posibilidad1 === posibilidad2){
         console.log("empate")
-    }
-    if (posibilidad1 === "piedra" && posibilidad2 === "papel" || 
+    } else if (posibilidad1 === "piedra" && posibilidad2 === "papel" || 
         posibilidad1 === "tijeras" && posibilidad2 === "piedra" ||
         posibilidad1 === "papel" && posibilidad2 === "tijeras") {
         console.log("Gana player 2")
-    }
-    if (posibilidad1 === "piedra" && posibilidad2 === "tijeras" ||
+    } else if (posibilidad1 === "piedra" && posibilidad2 === "tijeras" ||
         posibilidad1 === "papel" && posibilidad2 === "piedra" ||
         posibilidad1 === "tijeras" && posibilidad2 === "papel") {
         console.log("Gana player 1")
