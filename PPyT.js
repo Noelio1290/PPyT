a4d8f2318d32b37d56a8420e4ab6778bba70b32f
Noelio1290 <noelio1290@gmail.com>
2022-09-07 20:11:40 -0500

    Aprendimos a añadir escuchadores de eventos y a añadir y remover clases

--- PPyT.js
+++ PPyT.js
@@ -11,3 +11,21 @@ function jugarPPyT(posibilidad1,posibilidad2) {
         console.log("Gana player 1")
     }
 }
+function aprendiendoConClicks() {
+    console.log("Le di click")
+}
+const seleccionPapel = document.querySelector("#seleccionU")
+
+seleccionPapel.addEventListener("click",function(evento){
+    console.log("mande a llamar un click")
+    console.log(evento)
+    })
+const pagina1 = document.querySelector(".Pantalla-inicial")
+const pagina2 = document.querySelector(".Juego")
+const pagina3 = document.querySelector(".Resultado")
+
+console.log(pagina1,pagina2,pagina3)
+pagina1.classList.add("inactive") 
+pagina2.classList.add("inactive")
+pagina3.classList.add("inactive")
+pagina1.classList.remove("inactive")
