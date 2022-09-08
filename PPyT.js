d17b9390364346c20aab36d5d84b26f2d4fc6ffe
Noelio1290 <noelio1290@gmail.com>
2022-09-08 00:13:55 -0500

    ya puedo interactuar y moverme en mi app, falta conectar la funcion del juego al html y mis resultados

--- PPyT.js
+++ PPyT.js
@@ -11,6 +11,7 @@ function jugarPPyT(posibilidad1,posibilidad2) {
         console.log("Gana player 1")
     }
 }
+//notas
 function aprendiendoConClicks() {
     console.log("Le di click")
 }
@@ -20,12 +21,27 @@ seleccionPapel.addEventListener("click",function(evento){
     console.log("mande a llamar un click")
     console.log(evento)
     })
+    //Ejercicio
 const pagina1 = document.querySelector(".Pantalla-inicial")
 const pagina2 = document.querySelector(".Juego")
 const pagina3 = document.querySelector(".Resultado")
 
 console.log(pagina1,pagina2,pagina3)
-pagina1.classList.add("inactive") 
+
 pagina2.classList.add("inactive")
 pagina3.classList.add("inactive")
-pagina1.classList.remove("inactive")
+
+function pasarPagina2(){
+    pagina2.classList.remove("inactive")
+    pagina1.classList.add("inactive")
+}
+
+
+function pasarPagina3(){
+    pagina3.classList.remove("inactive")
+    pagina2.classList.add("inactive")
+}
+function paginaInicial(){
+    pagina1.classList.remove("inactive")
+    pagina3.classList.add("inactive")
+}
